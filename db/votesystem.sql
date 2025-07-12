f0a721cace8a8aeabadf5c39e2b3a823eec4ea67
KsTU-SRC <69836195+KaliBlip@users.noreply.github.com>
2025-07-12 15:01:22 +0000

    chore(db): update SQL dump to version 5.2.1; add new tables for login attempts and enhanced voter details; modify existing tables to include additional fields and adjust auto-increment values

--- db/votesystem.sql
+++ db/votesystem.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2021 at 02:28 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
+-- Generation Time: Jul 12, 2025 at 04:59 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,14 +35,14 @@ CREATE TABLE `admin` (
   `lastname` varchar(50) NOT NULL,
   `photo` varchar(150) NOT NULL,
   `created_on` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `firstname`, `lastname`, `photo`, `created_on`) VALUES
-(1, 'crce', '$2y$10$kLqXG4BAJrPbsOjJ/.B4eeZn6oojNhAb8l5/cb9eZvFnYU.pz2qni', 'CRCE', 'Admin', 'WhatsApp Image 2021-05-27 at 17.55.34.jpeg', '2018-04-02');
+(1, 'crce', '$2y$10$kLqXG4BAJrPbsOjJ/.B4eeZn6oojNhAb8l5/cb9eZvFnYU.pz2qni', 'CRCE', 'Admin', 'FCoZdtPXsAs2QUD.jpeg', '2018-04-02');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,36 @@ CREATE TABLE `candidates` (
   `lastname` varchar(30) NOT NULL,
   `photo` varchar(150) NOT NULL,
   `platform` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `candidates`
+--
+
+INSERT INTO `candidates` (`id`, `position_id`, `firstname`, `lastname`, `photo`, `platform`) VALUES
+(36, 21, 'Joshuah', 'Fargone', 'WhatsApp Image 2025-07-06 at 20.36.41_b2f4175f.jpg', 'Short message to students.'),
+(37, 19, 'Samuel', 'Kodom', 'WhatsApp Image 2025-07-06 at 20.36.41_3c781746.jpg', 'Short message to students.'),
+(38, 19, 'Paul', 'Gyamfi', 'WhatsApp Image 2025-07-06 at 20.36.42_df010d92.jpg', 'Short message to students.'),
+(39, 23, 'Shadrack', 'Marfo', 'WhatsApp Image 2025-07-06 at 20.36.40_7c5e18d5.jpg', 'Short message to students.'),
+(40, 20, 'Mary', 'MOB', 'WhatsApp Image 2025-07-06 at 20.36.42_40da08c5.jpg', 'Short message to students.'),
+(41, 19, 'Lamek', 'Sarfo', 'WhatsApp Image 2025-07-06 at 20.36.43_d129df0c.jpg', 'Short message to students.'),
+(42, 23, 'Isaac', 'Love', 'WhatsApp Image 2025-07-06 at 20.36.41_e81e7eee.jpg', 'Short message to students.'),
+(43, 24, 'Alhassan', 'A+A', 'WhatsApp Image 2025-07-06 at 20.36.42_97adbf33.jpg', 'Short message to students.'),
+(44, 22, 'Pearl', 'Taggo', 'WhatsApp Image 2025-07-06 at 20.36.43_f00e3bb2.jpg', 'Short message to students.'),
+(45, 21, 'Bible', 'Adjei', 'WhatsApp Image 2025-07-06 at 20.36.43_fbf4cda9.jpg', 'Short message'),
+(46, 21, 'Kofi', 'Kodzo', 'WhatsApp Image 2025-07-06 at 20.36.44_77eeee87.jpg', 'short message');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `attempt_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -69,8 +98,22 @@ CREATE TABLE `positions` (
   `id` int(11) NOT NULL,
   `description` varchar(50) NOT NULL,
   `max_vote` int(11) NOT NULL,
-  `priority` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `priority` int(11) NOT NULL,
+  `gender_class` varchar(100) NOT NULL,
+  `dept_category` varchar(10) NOT NULL DEFAULT 'all'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `positions`
+--
+
+INSERT INTO `positions` (`id`, `description`, `max_vote`, `priority`, `gender_class`, `dept_category`) VALUES
+(19, 'SRC PRESIDENT', 1, 1, 'all', 'all'),
+(20, 'SRC WOCOM', 1, 5, 'all', 'all'),
+(21, 'SRC PRO', 1, 4, 'all', 'all'),
+(22, 'SRC SECRETARY', 1, 2, 'all', 'all'),
+(23, 'GNUTS AMBASADOR', 1, 6, 'all', 'all'),
+(24, 'SRC TRESURER ', 1, 3, 'all', 'all');
 
 -- --------------------------------------------------------
 
@@ -84,8 +127,17 @@ CREATE TABLE `voters` (
   `password` varchar(60) NOT NULL,
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) NOT NULL,
-  `photo` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `photo` varchar(150) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `department` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `voters`
+--
+
+INSERT INTO `voters` (`id`, `voters_id`, `password`, `firstname`, `lastname`, `photo`, `gender`, `department`) VALUES
+(17, 'ctAbKY5HBdwJfQT', '$2y$10$JwyPqVrBF9Vu1lYYM4Vm6.qpr6BJkrD6KG9He753xbXn/AhpfW.aW', 'Darsh', 'Kofi', '1724405071466.png', 'male', 'Pasag');
 
 -- --------------------------------------------------------
 
@@ -97,8 +149,20 @@ CREATE TABLE `votes` (
   `id` int(11) NOT NULL,
   `voters_id` int(11) NOT NULL,
   `candidate_id` int(11) NOT NULL,
-  `position_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `position_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `voters_id`, `candidate_id`, `position_id`, `created_at`) VALUES
+(200, 17, 38, 19, '2025-07-07 18:13:10'),
+(201, 17, 40, 20, '2025-07-07 18:13:10'),
+(202, 17, 36, 21, '2025-07-07 18:13:10'),
+(203, 17, 39, 23, '2025-07-07 18:13:10'),
+(204, 17, 43, 24, '2025-07-07 18:13:10');
 
 --
 -- Indexes for dumped tables
@@ -115,6 +179,13 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `candidates`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ip_address` (`ip_address`);
 
 --
 -- Indexes for table `positions`
@@ -148,25 +219,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=205;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
